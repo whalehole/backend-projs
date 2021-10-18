7ed2a9c3be79d683c0af925559bbe1c8aef77d32
whalehole <68460417+whalehole@users.noreply.github.com>
2021-10-18 18:10:44 +0800

    update

---
+++ migrate_db.js
@@ -0,0 +1,57 @@
+const pool = require('./db/pool');
+
+pool.query(`CREATE TABLE public.user (
+	id BIGSERIAL PRIMARY KEY,
+	email VARCHAR(100) NOT NULL UNIQUE,
+	password VARCHAR(50) NOT NULL,
+	sub VARCHAR(200),
+    created_date DATE NOT NULL DEFAULT CURRENT_DATE,
+    is_deleted BOOLEAN DEFAULT false
+);`);
+
+
+pool.query(`CREATE TABLE public.todo_list (
+    id BIGSERIAL PRIMARY KEY,
+    owner_id BIGINT NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    todo_date DATE, 
+    created_date DATE NOT NULL DEFAULT CURRENT_DATE,
+    is_deleted BOOLEAN DEFAULT false,
+    CONSTRAINT fk_owner_id
+        FOREIGN KEY(owner_id)
+            REFERENCES public.user(id)
+            ON DELETE CASCADE
+);`);
+
+pool.query(`CREATE TABLE public.todo (
+    id BIGSERIAL PRIMARY KEY,
+    list_id BIGINT NOT NULL,
+    author_id BIGINT NOT NULL,
+    todo VARCHAR(1000) NOT NULL,
+    created_date DATE NOT NULL DEFAULT CURRENT_DATE,
+    is_deleted BOOLEAN DEFAULT false,
+    CONSTRAINT fk_list_id
+        FOREIGN KEY(list_id)
+            REFERENCES public.todo_list(id)
+            ON DELETE CASCADE,
+    CONSTRAINT fk_author_id
+        FOREIGN KEY(author_id)
+            REFERENCES public.user(id)
+            ON DELETE CASCADE
+);`);
+
+pool.query(`CREATE TABLE public.todolist_user_mapping (
+    user_id BIGINT NOT NULL,
+    todo_list_id BIGINT NOT NULL,
+    added_date DATE NOT NULL DEFAULT CURRENT_DATE,
+    is_deleted BOOLEAN DEFAULT FALSE,
+    PRIMARY KEY(user_id, todo_list_id),
+    CONSTRAINT fk_user
+        FOREIGN KEY(user_id)
+            REFERENCES public.user(id)
+            ON DELETE CASCADE,
+    CONSTRAINT fk_todo_list
+        FOREIGN KEY(todo_list_id)
+            REFERENCES public.todo_list(id)
+            ON DELETE CASCADE
+);`);
